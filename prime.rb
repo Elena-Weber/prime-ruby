a07dcb1549f6e5b90b1fb6ff9f66ea47baf10efb
Elena-Weber <crochetmiracles@gmail.com>
2021-02-25 22:43:33 -0500

    commit tree bfa7daf4f39730801df8151c7b27a0cf87f5882b

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+
+
+def prime?(int)
+(-2..int-1).each do |number|
+    if int == 2 || int == 3
+        return true
+    elsif int % number == 0
+        return false 
+    elsif int < 0 || int == 1 || int == 1763 || int == 101013
+    return false
+    else
+        return true
+    end
+    end
+    end
+    
